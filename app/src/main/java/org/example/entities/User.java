8eec0d25c74ab64398c154b989db8a70e697ccd1
nikkk404 <nikhil20mokal@gmail.com>
2025-03-26 21:53:55 +0530

    first

---
+++ app/src/main/java/org/example/entities/User.java
@@ -0,0 +1,9 @@
+package org.example.entities;
+
+public class User {
+    private String name;
+    private String password;
+    private String hashPassword;
+    private List<Ticket> ticketsBooked;
+    private String userId;
+}
